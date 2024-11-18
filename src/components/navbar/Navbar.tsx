d430c19150d5ee9e59802c897ba41538d1132ef8
Jonas <jonasneu812@gmail.com>
2024-11-18 16:17:22 +0100

    navbar cleanup

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,124 +2,50 @@ import React, { useState } from 'react';
 import styles from './Navbar.module.css';
 import { Link, useLocation } from 'react-router-dom';
 
-interface Category {
-    gender?: string;
-    categoryRoute?: string;
-    categoryName?: string;
-}
-
-const categories: Category[] = [
+const navData = [
     {
-        gender: "MEN",
-        categoryRoute: "/men/jeans",
-        categoryName: "Jeans"
+        category: "MEN",
+        subCategories: ["New Arrivals", "Jackets", "Knit", "Shirts", "Pants"],
     },
     {
-        gender: "MEN",
-        categoryRoute: "/men/shirts",
-        categoryName: "Shirts"
+        category: "WOMEN",
+        subCategories: ["New Arrivals", "Jackets", "Knit", "Tops", "Pants", "Dresses"],
     },
     {
-        gender: "MEN",
-        categoryRoute: "/men/shorts",
-        categoryName: "Shorts"
+        category: "ACCESSOIRES",
+        subCategories: ["Bags", "Headwear", "Jewelry"],
     },
-    {
-        gender: "MEN",
-        categoryRoute: "/men/shirts",
-        categoryName: "Accessoires"
-    },
-    {
-        gender: "MEN",
-        categoryRoute: "/men/shorts",
-        categoryName: "Beachwear"
-    },
-    {
-        gender: "WOMEN",
-        categoryRoute: "/women/headwear",
-        categoryName: "Headwear"
-    },
-    {
-        gender: "WOMEN",
-        categoryRoute: "/women/shirts",
-        categoryName: "Shirts"
-    },
-    {
-        gender: "WOMEN",
-        categoryRoute: "/women/knits",
-        categoryName: "Knits"
-    },
-    {
-        gender: "ACCESSOIRES",
-        categoryRoute: "/accessoires/glasses",
-        categoryName: "Glasses"
-    },
-    {
-        gender: "ACCESSOIRES",
-        categoryRoute: "/accessoires/jewelry",
-        categoryName: "Jewelry"
-    }
 ];
 
 const Navbar = () => {
-    const [hoveredText, setHoveredText] = useState<string | null>(null);
-    const [isHoveringCategory, setIsHoveringCategory] = useState(false);
-
-    const handleMouseEnter = (text: string) => {
-        setHoveredText(text);
-        setIsHoveringCategory(false);
-    };
-
-    const handleMouseLeave = () => {
-        setIsHoveringCategory(true);
-    };
-
-    const handleCategoryMouseEnter = () => setIsHoveringCategory(true);
-    const handleCategoryMouseLeave = () => {
-        setIsHoveringCategory(false);
-        setHoveredText(null);
-    };
-
-    const location = useLocation();
-    const currentPath = location.pathname
+    const [category, setCategory] = useState<string | null>(null);
 
     return (
         <div className={styles.headerContainer}>
             <div className={styles.navbarContainer}>
-                <Link to="/" onMouseEnter={handleCategoryMouseLeave}>
                     <div id={styles.logoContainer}>
-                        <img src="./volume.svg" alt="volume" id={styles.logo} />
+                        <Link to="/">
+                            <img src="./volume.svg" 
+                            alt="volume" 
+                            id={styles.logo}
+                            onMouseEnter={() => setCategory(null)} />
+                        </Link>
                     </div>
-                </Link>
 
                 <div className={styles.navbarTextContainer}>
+                {navData.map((item) => 
                     <Link
-                        to="/men"
-                        className={styles.navbarText}
-                        onMouseEnter={() => handleMouseEnter('MEN')}
-                        onMouseLeave={handleMouseLeave}
-                    >
-                        MEN
-                    </Link>
-                    <Link
-                        to="/women"
-                        className={styles.navbarText}
-                        onMouseEnter={() => handleMouseEnter('WOMEN')}
-                        onMouseLeave={handleMouseLeave}
-                    >
-                        WOMEN
-                    </Link>
-                    <Link
-                        to="/accessoires"
-                        className={styles.navbarText}
-                        onMouseEnter={() => handleMouseEnter('ACCESSOIRES')}
-                        onMouseLeave={handleMouseLeave}
-                    >
-                        ACCESSOIRES
-                    </Link>
+                    to={`${item.category.toLowerCase()}`}
+                    className={styles.navbarText}
+                    onMouseEnter={() => setCategory(item.category)}
+                >
+                    {item.category}
+                </Link>
+                )}
                 </div>
 
-                <div id={styles.navbarIconContainer} onMouseEnter={handleCategoryMouseLeave}>
+                <div id={styles.navbarIconContainer}
+                onMouseEnter={() => setCategory(null)}>
                     <span id={styles.searchBox}>
                         <input type="text" id={styles.searchInput}/>
                         <img src="./searchIcon.svg" alt="search" className={styles.navbarIcon} />
@@ -135,22 +61,24 @@ const Navbar = () => {
                 </div>
             </div>
 
-            {(hoveredText || isHoveringCategory) && (
+            {(category != null) && (
                 <div
                     className={styles.kategorieContainer}
-                    onMouseEnter={handleCategoryMouseEnter}
-                    onMouseLeave={handleCategoryMouseLeave}>
-                    <span id={styles.categoryNameBox}>{currentPath.slice(1)}</span>
+                    onMouseLeave={() => setCategory(null)}>
+                    <span id={styles.categoryNameBox}></span>
 
                     <div className={styles.kategoriesBox}>
-                        {categories
-                            .filter(category => category.gender === hoveredText)
-                            .map((category, index) => (
-                                <Link key={index} to={category.categoryRoute as string} className={styles.navbarText}>
-                                    {category.categoryName}
-                                </Link>
-                            ))
-                        }
+                    {navData
+                        .find((item) => item.category === category)
+                        ?.subCategories.map((subCategory, index) => (
+                            <Link
+                            key={index}
+                            to={`/${category.toLowerCase()}/${subCategory.toLowerCase().replace(/ /g, "-")}`}
+                            className={styles.navbarText}
+                            >
+                            {subCategory}
+                            </Link>
+                        ))}
                     </div>
                 </div>
             )}
