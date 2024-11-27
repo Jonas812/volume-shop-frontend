d155aa37822e645544af0bfa3a29aef070225442
Jonas <jonasneu812@gmail.com>
2024-11-27 23:58:29 +0100

    homepage banner

--- src/pages/home/Homepage.tsx
+++ src/pages/home/Homepage.tsx
@@ -1,17 +1,29 @@
 import styles from "./Homepage.module.css"
 import ProductSlider from "../../components/productSlider/ProductSlider"
+import { useState } from "react";
 
 function Homepage() {
   
     return (
       <>
         <div className={styles.banner}>
-          <div className={styles.bannerOverlay}/>
-        </div>
+          <div id={styles.innerBannerContainer}>
+            <div id={styles.bannerTextBox}>
+            <span className={styles.lightHeaderText} id={styles.collectionBannerHeader}>Lookbook</span>
+              <span className={styles.lightRegularText} id={styles.collectionBannerText}>Winter 2024</span>
+           
+            </div>
+            <div id={styles.buttonBox}>
+              <button className={styles.accentButton}>MEN</button>
+              <button className={styles.accentButton}>WOMEN</button>
+            </div>
+          </div>
+        </div> 
+
         <div className={styles.shippingBannerContainer}>
             <div className={styles.shippingBanner}>
-                <p>KOSTENLOSER VERSAND ÜBER 99€</p>
-                <p id={styles.smallerText}>Mindestbestellwert 20€</p>
+                <span className={styles.regularText}>KOSTENLOSER VERSAND ÜBER 99€</span>
+                <span className={styles.smallText}>Mindestbestellwert 20€</span>
             </div>
         </div>
         <ProductSlider></ProductSlider>
