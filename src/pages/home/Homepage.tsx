2abfcc5c34effad5f773604f8c07be6ec311dacb
Jonas <jonasneu812@gmail.com>
2024-11-28 20:28:59 +0100

    links in banner

--- src/pages/home/Homepage.tsx
+++ src/pages/home/Homepage.tsx
@@ -1,6 +1,7 @@
 import styles from "./Homepage.module.css"
 import ProductSlider from "../../components/productSlider/ProductSlider"
 import { useState } from "react";
+import { Link } from 'react-router-dom';
 
 function Homepage() {
   
@@ -9,12 +10,16 @@ function Homepage() {
         <div className={styles.banner}>
           <div id={styles.innerBannerContainer}>
             <div id={styles.bannerTextBox}>
-            <span className={styles.lightHeaderText} id={styles.collectionBannerHeader}>Lookbook</span>
+              <span className={styles.lightHeaderText} id={styles.collectionBannerHeader}>Lookbook</span>
               <span className={styles.lightRegularText} id={styles.collectionBannerText}>Winter 2024</span>
            
               <div id={styles.buttonBox}>
+              <Link className={styles.link} to="/men">
                 <button className={styles.accentButton}>MEN</button>
+              </Link>
+              <Link className={styles.link} to="/women">
                 <button className={styles.accentButton}>WOMEN</button>
+              </Link>
               </div>
             </div>
 
